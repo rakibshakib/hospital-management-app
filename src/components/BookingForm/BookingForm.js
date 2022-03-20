372ddee7205028bd68adb3cf0a44c0b575ad6816
rakibshakib <rakibshakib74@gmail.com>
2022-03-21 01:40:06 +0600

    added form

--- src/components/BookingForm/BookingForm.js
+++ src/components/BookingForm/BookingForm.js
@@ -1,7 +1,64 @@
+import TextField from '@mui/material/TextField';
 import React from 'react';
-
 const BookingForm = () => {
-    return <div>BookingForm</div>;
+    const handleAppoinment = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div>
+            <h2>BOOK APPOINTMENT</h2>
+            <form action="" onSubmit={handleAppoinment}>
+                <TextField
+                    id="outlined-basic"
+                    label="Patient Name"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="filled"
+                    type="text"
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Phone Number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="filled"
+                    type="number"
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Choose Date"
+                    type="date"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="filled"
+                />
+                <TextField
+                    select
+                    id="outlined-basic"
+                    label="Choose Doctor"
+                    type="date"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="filled"
+                    SelectProps={{
+                        native: true,
+                    }}
+                    onChange={(e) => console.log(e.target.value)}
+                >
+                    <option value="Dr.Veranda Tanumihardja">
+                        Dr.Veranda Tanumihardja
+                    </option>
+                    <option value="Dr. Kathryn Wood">Dr. Kathryn Wood</option>
+                    <option value="Dr. John Allen">Dr. John Allen</option>
+                    <option value="Dr. Peter Parker">Dr. Peter Parker</option>
+                </TextField>
+            </form>
+        </div>
+    );
 };
 
 export default BookingForm;
--- src/components/Service/ServiceDetails.js
+++ src/components/Service/ServiceDetails.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import BookingForm from '../BookingForm/BookingForm';
 // import useHospitalData from '../../Hooks/useHospitalData';
 
 const ServiceDetails = () => {
@@ -57,6 +58,9 @@ const ServiceDetails = () => {
                         </div>
                     </div>
                 )}
+            </div>
+            <div className="my-12">
+                <BookingForm />
             </div>
         </div>
     );
